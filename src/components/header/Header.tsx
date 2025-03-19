94a6df1a06baefdbb48a1ea5255b3d134686dd9c
Julia T <juliaturkali@gmail.com>
2025-03-19 14:58:50 +0100

    Add welcome message to Header component for home page

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,9 +1,16 @@
+import { useLocation } from "react-router-dom";
 import GenreFilter from "../genreFilter/GenreFilter";
 import SearchBar from "../searchBar/SearchBar";
 
 export default function Header() {
+  const location = useLocation();
   return (
     <header className="mx-6 my-10 flex flex-col gap-4">
+      {location.pathname === "/home" && (
+        <h1 className="mt-5 mb-10 text-center text-4xl font-bold text-gray-950">
+          Welcome!
+        </h1>
+      )}
       <SearchBar />
       <GenreFilter />
     </header>
