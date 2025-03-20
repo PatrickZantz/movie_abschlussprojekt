ae6c7df4ea35c5ab55f9321539b750b80063ab60
Julia T <juliaturkali@gmail.com>
2025-03-20 15:39:10 +0100

    Refactor Footer component to not exceed w-full

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,9 +1,13 @@
-
-import React, { useState } from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faHome, faBookmark, faUser, faDownload, } from '@fortawesome/free-solid-svg-icons';
-import { useNavigate, useLocation } from 'react-router-dom';
-import EllipseImage from '../../assets/Ellipse 7.png';
+import React, { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faHome,
+  faBookmark,
+  faUser,
+  faDownload,
+} from "@fortawesome/free-solid-svg-icons";
+import { useNavigate, useLocation } from "react-router-dom";
+import EllipseImage from "../../assets/Ellipse 7.png";
 
 const Footer = () => {
   const navigate = useNavigate();
@@ -13,11 +17,9 @@ const Footer = () => {
   // const [showFavorites, setShowFavorites] = useState(false);
 
   const handleIconClick = (icon: string) => {
-    
     setActiveIcon(icon);
-    if (icon === 'home') {
-      navigate('/home');
-
+    if (icon === "home") {
+      navigate("/home");
     }
   };
 
@@ -30,50 +32,67 @@ const Footer = () => {
   //   setActiveIcon('bookmark');
   // };
 
-
-
   const getIconClass = (icon: string) => {
-    return activeIcon === icon ? 'active-icon' : '';
+    return activeIcon === icon ? "active-icon" : "";
   };
 
-  const isHomePage = location.pathname === '/home';
-
-
+  const isHomePage = location.pathname === "/home";
 
   return (
-    <div className='bg-gray-50 py-2 sticky bottom-0 left-0 right-0 w-full'>
+    <div className="sticky right-0 bottom-0 left-0 w-full bg-white py-4">
+      <footer className="flex items-center justify-between px-10 text-[#97aabd]">
+        <div
+          onClick={() => handleIconClick("home")}
+          className={`relativ} relative flex cursor-pointer items-center`}
+        >
+          <img
+            src={EllipseImage}
+            alt="Ellipse"
+            className={`absolute top-1/2 left-[-10px] z-0 h-4 w-4 -translate-y-1/2 transform text-[#97aabd] ${isHomePage ? "text-red-400" : ""}`}
+          />
 
-    <footer className="flex gap-22 items-center justify-center s text-[#97aabd]">
-
-        <div onClick={() => handleIconClick('home')} className={`flex items-center cursor-pointer relativ} relative`}>
-          
-          <img 
-            src={EllipseImage} 
-            alt="Ellipse" 
-            className={`w-4 h-4 absolute left-[-10px] top-1/2 transform -translate-y-1/2 z-0 text-[#97aabd] ${isHomePage ? 'text-red-400' : ''}`}
-/>
-          
-            <FontAwesomeIcon 
-            icon={faHome} 
-            className={`text-xl z-10 relative ${isHomePage ? 'text-red-400' : 'text-[#97aabd]'}`}/>
-          <p className={`font-bold text-l ml-4 ${isHomePage ? 'text-red-500' : ''}`}>Home</p>
+          <FontAwesomeIcon
+            icon={faHome}
+            className={`relative z-10 text-xl ${isHomePage ? "text-red-400" : "text-[#97aabd]"}`}
+          />
+          <p
+            className={`text-l ml-4 font-bold ${isHomePage ? "text-red-500" : ""}`}
+          >
+            Home
+          </p>
         </div>
 
-        <div onClick={() => handleIconClick('download')} 
-          className={`cursor-pointer`}>
-          <FontAwesomeIcon icon={faDownload} className={`text-xl ${isIconActive('download') ? 'text-red-400' : 'text-[#97aabd]'}`} />
+        <div
+          onClick={() => handleIconClick("download")}
+          className={`cursor-pointer`}
+        >
+          <FontAwesomeIcon
+            icon={faDownload}
+            className={`text-xl ${isIconActive("download") ? "text-red-400" : "text-[#97aabd]"}`}
+          />
         </div>
 
-        <div onClick={() => handleIconClick('bookmark')} className={`cursor-pointer`}>
-          <FontAwesomeIcon icon={faBookmark} className={`text-xl ${isIconActive('bookmark') ? 'text-red-400' : 'text-[#97aabd]'}`} />
-        </div>
-        
-        <div onClick={() => handleIconClick('profile')} className={`cursor-pointer`}>
-          <FontAwesomeIcon icon={faUser} className={`text-xl ${isIconActive('profile') ? 'text-red-400' : 'text-[#97aabd]'}`} />
+        <div
+          onClick={() => handleIconClick("bookmark")}
+          className={`cursor-pointer`}
+        >
+          <FontAwesomeIcon
+            icon={faBookmark}
+            className={`text-xl ${isIconActive("bookmark") ? "text-red-400" : "text-[#97aabd]"}`}
+          />
         </div>
 
-    </footer>
-            </div>
+        <div
+          onClick={() => handleIconClick("profile")}
+          className={`cursor-pointer`}
+        >
+          <FontAwesomeIcon
+            icon={faUser}
+            className={`text-xl ${isIconActive("profile") ? "text-red-400" : "text-[#97aabd]"}`}
+          />
+        </div>
+      </footer>
+    </div>
   );
 };
 
