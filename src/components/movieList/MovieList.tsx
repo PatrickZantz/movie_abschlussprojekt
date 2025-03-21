88c2b9d4617d9edb5875597838689be51320e6ff
PatrickZantz <p.zantz@gmail.com>
2025-03-21 11:42:26 -0300

    unnötiger header

--- src/components/movieList/MovieList.tsx
+++ src/components/movieList/MovieList.tsx
@@ -52,9 +52,6 @@ export default function MovieList() {
 
   return (
     <div className="p-6 bg-gray-100 min-h-screen">
-      <h1 className="text-2xl font-bold mb-6">
-        {searchString ? `Suchergebnisse für "${searchString}"` : 'Beliebte Filme'}
-      </h1>
 
       {/* Fehlermeldung */}
       {error && (
