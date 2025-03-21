1b38834b7b36407940d200b645c35ca0a3f0baac
PatrickZantz <p.zantz@gmail.com>
2025-03-21 11:12:46 -0300

    disabled wieder raus

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -4,16 +4,14 @@ type ButtonProps = {
   children: React.ReactNode;
   onClick: () => void;
   className?: string;
-  disabled?: boolean;
   isActive?: boolean;
 };
 
-const Button: React.FC<ButtonProps> = ({ children, className, onClick, disabled, isActive = false }) => {
+const Button: React.FC<ButtonProps> = ({ children, className, onClick, isActive = false }) => {
   return (
     <button
       onClick={onClick}
-      disabled={disabled}
-      className={`flex min-w-fit items-center justify-center rounded-md py-3 text-gray-950 ${isActive ? "bg-primary text-white" : "bg-secondary"} ${disabled ? "opacity-50 cursor-not-allowed" : ""} ${className || ""}`}
+      className={`flex min-w-fit items-center justify-center rounded-md py-3 text-gray-950 ${isActive ? "bg-primary text-white" : "bg-secondary"} ${className || ""}`}
     >
       {children}
     </button>
