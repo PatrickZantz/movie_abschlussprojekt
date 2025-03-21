11f8aff1588e6ce7e33bc4d3c7314e5233c7983e
Julia T <juliaturkali@gmail.com>
2025-03-21 12:16:03 +0100

    Update Header component to improve sticky behavior and adjust displayed title based on route

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -6,14 +6,14 @@ export default function Header() {
   const location = useLocation();
   return (
     <header
-      className={`flex w-full flex-col gap-2 px-6 pt-5 ${location.pathname !== "/home" && "sticky bg-white shadow-lg"}`}
+      className={`z-10 flex w-full flex-col gap-2 px-6 pt-5 ${location.pathname !== "/home" && "sticky top-0 bg-white shadow-lg"}`}
     >
       {location.pathname === "/home" && (
         <h1 className="mt-10 mb-10 text-center text-4xl font-bold text-gray-950">
           Welcome!
         </h1>
       )}
-      {location.pathname === "/search" && (
+      {location.pathname !== "/home" && (
         <h1 className="text-primary text-center text-2xl font-bold">.MOV</h1>
       )}
       <SearchBar />
