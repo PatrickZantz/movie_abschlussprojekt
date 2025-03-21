87f61c696bb46c566d1adead94d37bca1572edd9
PatrickZantz <p.zantz@gmail.com>
2025-03-21 11:53:04 -0300

    trending weiter hoch

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -3,7 +3,7 @@ import TrendingMovies from "../../components/trendingMovies/TrendingMovies";
 const HomePage = () => {
   return (
     <section className="searchHome">
-      <div className="mt-30">
+      <div className="mt-[10%]">
         <TrendingMovies />
       </div>
     </section>
